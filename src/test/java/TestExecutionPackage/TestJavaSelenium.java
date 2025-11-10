54698f91988994906c41d6fa993338bd0fa6e234
Manjunath <mgadigii@yahoo.co.in>
2025-11-10 17:51:23 +0530

    commit to git hub

--- src/test/java/TestExecutionPackage/TestJavaSelenium.java
+++ src/test/java/TestExecutionPackage/TestJavaSelenium.java
@@ -1,10 +1,13 @@
 package TestExecutionPackage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -71,11 +74,36 @@ public class TestJavaSelenium {
                 driver.findElement(By.cssSelector("span#hrefIncAdt")).click();
                 i++;
             }
-
-
-
         }
         driver.quit();
+    }
+
+    // Dynamic Dropdown handling example
+    @Test
+    public void SelectFrom_To_dynamicDropdown() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); // prefer explicit waits
+            driver.manage().window().maximize();
+            driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+            Thread.sleep(3000);
+            WebElement element = driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXTaction"));
+            element.click();
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[value='BLR']")));
+            driver.findElement(By.cssSelector("a[value='BLR']")).click();
+           /* we can perform click operation using javascript executor also
+            WebElement we = driver.findElement(By.cssSelector("a[value='BLR']"));
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();",we);*/
+            Thread.sleep(2000);
+            driver.findElement(By.partialLinkText("MAA")).click();
+
+        }
+        finally {
+            Thread.sleep(3000);
+            driver.quit();
+        }
     }
 }
 
