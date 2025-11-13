8aa4c969b2fbf46d8710b273892a7613d8d99fb4
Manjunath <mgadigii@yahoo.co.in>
2025-11-13 14:06:14 +0530

    Merge Conflicts

--- src/test/java/TestExecutionPackage/TestJavaSelenium.java
+++ src/test/java/TestExecutionPackage/TestJavaSelenium.java
@@ -1,7 +1,6 @@
 package TestExecutionPackage;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
