ecafdbe85b3501dd24f4b3b28304405544d78677
Manjunath <mgadigii@yahoo.co.in>
2025-11-13 13:43:24 +0530

    commit to git hub

--- src/test/java/TestExecutionPackage/TestJavaSelenium.java
+++ src/test/java/TestExecutionPackage/TestJavaSelenium.java
@@ -1,7 +1,7 @@
 package TestExecutionPackage;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
